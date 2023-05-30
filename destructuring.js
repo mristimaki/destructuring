ee09823ccd217b59f3f4a7121b46f3e768007216
mristimaki <rosanna.ristimaki@gmail.com>
2023-05-30 04:00:19 +0000

    Initial commit

--- destructuring.js
+++ destructuring.js
@@ -3,10 +3,62 @@
  * command node destructuring.js in the terminal
  */
 
-// Destructuring arrays
+// Destructuring arrays (commented out code does not use destructuring)
+let ages = [30, 26, 27];
+// let john = ages[0];
+// let mary = ages[1];
+// let joe = ages[2];
+let [john, mary, joe] = ages;
+
+console.log(john, mary, joe);
 
 // Destructuring objects
+let jobs = {
+    mike: "designer",
+    jill: "developer", 
+    alicia: "accountant",
+};
+let { mike, jill, alicia } = jobs;
+
+console.log(mike, jill, alicia);
+
+// Note that in both of the above situations,
+// while it looks like you're defining an array or object,
+// by putting the square brackets or curly braces on the 
+// left side of the expression, ES6 understands that you're
+// trying to create three separate variables using destructuring
 
 // Destructuring subsets
+let languages = ["english", "french", "spanish", "german", "japanese"];
+let [johnNative, johnSecondary] = languages;
+console.log(johnNative, johnSecondary);
+
+let [, , maryNative, marySecondary] = languages;
+console.log(maryNative, marySecondary);
+
+let languages2 = {
+    firstLanguage: "english",
+    secondLanguage: "french",
+    thirdLanguage: "german",
+    fourthLanguage: "japanese",
+};
+let { firstLanguage, thirdLanguage } = languages2;
+console.log(firstLanguage, thirdLanguage);
 
 // Using rest parameter syntax
+let fruits = ["apple", "orange", "banana", "peach", "cherry"];
+let [favourite, secondFavourite, ...others] = fruits;
+console.log(favourite);
+console.log(secondFavourite);
+console.log(others);
+
+let favouriteFoods = {
+    brian: "pizza",
+    anna: "pasta",
+    sarah: "vegetarian",
+    andrea: "steak"
+};
+let {brian, anna, ...rest} = favouriteFoods;
+console.log(brian);
+console.log(anna);
+console.log(rest);
